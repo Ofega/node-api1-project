98e727cc1f36d68fe927435c2dc40be82525a0e6
ofega <fegaeze@gmail.com>
2019-10-28 17:40:11 +0100

    set up express app

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// implement your API here
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+
+server.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('server listening on port ' + (process.env.PORT || 3000))
+})
