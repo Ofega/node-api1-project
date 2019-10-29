3a84d57bf010c1966bd17a292479ed9fba3cec91
ofega <fegaeze@gmail.com>
2019-10-29 07:34:09 +0100

    Can get all users

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./data/db');
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.get('/api/users', getAllUsers);
+server.get('/api/users/:id', getAUser);
 
-server.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+function getAllUsers(req, res) {
+    db.find()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.json(error)
+        })
+}
+
+function getAUser(req, res) {
+    db.find()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.json(error)
+        })
+}
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('server listening on port ' + (process.env.PORT || 3000))
